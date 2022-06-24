6117ed96707355b7d380dc5a80c5e2a2ebeb75f6
David Feeley <davidpfeeley@gmail.com>
2022-06-24 14:40:52 -0700

    Working on ns_but_pad_times check

--- src/meetchecker/checkers/ns_but_pad_times.py
+++ src/meetchecker/checkers/ns_but_pad_times.py
@@ -2,13 +2,17 @@ from .base import BaseChecker
 
 
 class Checker(BaseChecker):
+    def __init__(self, num_times=1, **kwargs):
+        super().__init__(**kwargs)
+        self.num_times = num_times
+
     def get_reason(self, row):
         return f"NS but yet we got {row.num_pad_times} electronic time(s)"
 
     def check(self, data):
         entry = data["entry"]
+        breakpoint()
         return entry.loc[
             ((entry.fin_stat == "R") | (entry.fin_heat == 0))
-            & (entry.fin_stat != "Q")
-            & (entry.num_pad_times > 0)
+            & (entry.num_pad_times >= self.num_times)
         ]
